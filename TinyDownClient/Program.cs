8e2a8b417ee52b34dc9caf3cde12756ba907ea6d
kesshei <dagang520@qq.com>
2024-05-29 10:32:59 +0800

    先增加个命令行解析

---
+++ TinyDownClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace TinyDownClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+              .AddCommandLine(args)
+              .Build();
+
+            Console.WriteLine($"path: {config["path"]}");
+            Console.WriteLine($"other: {config["other"]}");
+            Console.ReadLine();
+        }
+    }
+}
